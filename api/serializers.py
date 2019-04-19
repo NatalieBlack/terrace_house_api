b326c3275a0e16f83a77fe2df28658416faa543b
NatalieBlack <natalievictoriablack@gmail.com>
2019-04-19 17:05:43 -0400

    Update field names in serializer

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Member 
-        fields = ('name_en', 'name_jp', 'series', 'start_week', 'end_week')
+        fields = ('full_name_en', 'full_name_jp', 'nickname_en', 'nickname_jp', 'series', 'start_week', 'end_week')
 
 
 class SeriesSerializer(serializers.HyperlinkedModelSerializer):
