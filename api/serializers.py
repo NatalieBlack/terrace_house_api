f937ea0cbb56f26ae29f48d4ff3bd0af1ef3252d
NatalieBlack <natalievictoriablack@gmail.com>
2019-04-23 20:50:49 -0400

    Add primary key to serialized fields

--- api/serializers.py
+++ api/serializers.py
@@ -3,12 +3,13 @@ from rest_framework import serializers
 
 
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
+
     class Meta:
         model = Member 
-        fields = ('full_name_en', 'full_name_jp', 'nickname_en', 'nickname_jp', 'series', 'start_week', 'end_week')
+        fields = ('id', 'full_name_en', 'full_name_jp', 'nickname_en', 'nickname_jp', 'series', 'start_week', 'end_week')
 
 
 class SeriesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Series
-        fields = ('name', 'number')
+        fields = ('id', 'name', 'number')
